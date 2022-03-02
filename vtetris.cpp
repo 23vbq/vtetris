b8c28f24e1ea9a39a8e97748673eae46162e9757
Vblacqe <vblackgp123@gmail.com>
2022-03-02 21:31:27 +0100

    wstepne

---
+++ vtetris.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+/*#include <chrono>
+#include <thread>*/
+
+using namespace std;
+
+#define WIDTH 21
+#define HEIGHT 21
+#define borderSymbol "#"
+#define SPEED 0.5
+
+bool renderChange = true;
+short p[HEIGHT][WIDTH] = {{0}};
+short offset = 0;
+
+void moveLeft()
+{
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
+        if (p[i][0])
+        {
+            return;
+        }
+    }
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
+        for (int j = 1; j < WIDTH; j++)
+        {
+            p[i][j - 1] = p[i][j];
+        }
+        p[i][WIDTH - 1] = 0;
+    }
+    renderChange = true;
+    return;
+}
+void moveRight()
+{
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
+        if (p[i][HEIGHT - 1])
+        {
+            return;
+        }
+    }
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
+        for (int j = WIDTH - 2; j >= 0; j--)
+        {
+            p[i][j + 1] = p[i][j];
+        }
+        p[i][0] = 0;
+    }
+    renderChange = true;
+    return;
+}
+
+void goDown()
+{
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
+        // p[i] = p[i - 1];
+        for (int j = 0; j < WIDTH - 2; j++)
+        {
+            p[i][j] = p[i - 1][j];
+        }
+    }
+    // p[0] = {0};
+    renderChange = true;
+    Sleep(1000 * SPEED);
+    return;
+}
+
+void render()
+{
+    system("cls");
+    for (int i = 0; i < HEIGHT + 2; i++)
+    {
+        for (int j = 0; j < WIDTH + 2; j++)
+        {
+            if (i == 0 || i == (HEIGHT + 2) - 1 || j == 0 || j == (WIDTH + 2) - 1)
+            {
+                cout << borderSymbol;
+            }
+            else if (p[i - 1][j - 1] == 1)
+            {
+                cout << "0";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    renderChange = false;
+}
+
+int main()
+{
+    p[0][10] = 1;
+    render();
+    while (true)
+    {
+        goDown();
+        int handler = getch();
+        // cout << handler << endl;
+        if (handler == 97) // A
+        {
+            // cout << "Lewo" << endl;
+            moveLeft();
+        }
+        else if (handler == 100) // D
+        {
+            // cout << "Prawo" << endl;
+            moveRight();
+        }
+        else if (handler == 119)
+        {
+            // w
+            cout << "Gora" << endl;
+        }
+        else if (handler == 115)
+        {
+            // s
+            cout << "Dol" << endl;
+        }
+        else if (handler == 113) // Q - quit
+        {
+            return 0;
+        }
+        if (renderChange)
+        {
+            render();
+        }
+    }
+}
